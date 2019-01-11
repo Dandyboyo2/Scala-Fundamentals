38c45888d30c72975c775619dfa0c8ed7c8b42f7
Dan <daniel.alvey@digital.hmrc.gov.uk>
2019-01-11 09:54:23 +0000

    uploaded personal code

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -1,5 +1,63 @@
 package app
 
+import scala.io.StdIn
+
 object GreeterApplication extends App {
-  println("Hello!")
+
+  def greet(name: String): Unit = println(s"Hello $name")
+
+  val name = StdIn.readLine("Hello, what is your name?\n")
+
+  greet(name)
+
+  def subtract()
+
+    StdIn.readLine("How old are you?\n")
+
+    def age(year: Int): Unit = StdIn.readLine("That's old, I am 2\n")
+
+    val year = println("That's old, I am 2\n")
+
+    def play(): Boolean = StdIn.readLine("Would you like to play a game? True/False\n").toBoolean
+
+    if (play) {
+      println("Ok, Lets Begin!")
+    }
+
+    else println("Ok, maybe some other time!")
+
+    else if (play) != Boolean println("Sorry, please reply with True/False.")
+
+
+    val RandomNumGen = scala.util.Random
+    val x = RandomNumGen.nextInt(100) + 1B
+
+    var y = StdIn.readLine("Enter a number\n").toInt
+
+    println("You entered\n" + y)
+
+    while (x != y) {
+
+      if (x > y) {
+        println("too low")
+      }
+
+      if (x < y) {
+        println("too high")
+      }
+      y = StdIn.readLine("Enter a number\n").toInt
+
+    }
+    println("congratulations")
+
+
+  def scala(young : Int) : Int  Int
+
+  val(young) = year*
+
 }
+
+
+
+
+
